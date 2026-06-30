6a33a2f076cdfdc29d8aee35f2c3382415e7aacf
alisa12405 <qyenhoaqha124@gmail.com>
2026-06-30 18:50:04 +0000

    Update example file

---
+++ api/config/config.example.php
@@ -0,0 +1,7 @@
+<?php
+define('BASEURL', 'https://your-domain.com/');
+define('DB_HOST', 'db');
+define('DB_NAME', 'your_database');
+define('DB_USER', 'your_username');
+define('DB_PASS', 'your_password');
+define('DB_CHARSET', 'utf8mb4');
