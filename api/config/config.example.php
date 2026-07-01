bb8f26351497ea2754817629fc5ac418c8115fd7
Trung Hieu <phamhoangtrunghieu1@gmail.com>
2026-07-01 05:48:38 +0000

    sua password dtb main

--- api/config/config.example.php
+++ api/config/config.example.php
@@ -3,5 +3,5 @@ define('BASEURL', 'https://your-domain.com/');
 define('DB_HOST', 'db');
 define('DB_NAME', 'your_database');
 define('DB_USER', 'your_username');
-define('DB_PASS', 'your_password');
+define('DB_PASS', '123456');
 define('DB_CHARSET', 'utf8mb4');
