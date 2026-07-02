a41fcf4c21d5ffc35f0f6ce55a70596b399e04a8
alisa12405 <qyenhoaqha124@gmail.com>
2026-07-02 00:45:53 +0000

    Change title

--- pages/index2.php
+++ pages/index2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang chủ PHP</title>
+    <title>Trang chủ Apache PHP</title>
     <style>
         body {
             font-family: Arial, sans-serif;
